0a76eb982de917d65043f2582a9ae210d9b2bf28
1ntellekt <pitetvasa@gmail.com>
2022-08-26 10:59:03 +0600

    add pool requests to WorkerController.php

--- app/Http/Controllers/Web/getSetting/WorkerController.php
+++ app/Http/Controllers/Web/getSetting/WorkerController.php
@@ -7,7 +7,9 @@ use App\Http\Controllers\Controller;
 use App\Http\Controllers\getData\getDevices;
 use App\Http\Controllers\getData\getSetting;
 use App\Http\Controllers\getData\getWorkers;
+use Illuminate\Http\Client\Pool;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class WorkerController extends Controller
 {
@@ -22,7 +24,12 @@ class WorkerController extends Controller
             $Workers = new getWorkers($item->znm);
         }
 
-        if ($Workers != null) $Workers = $Workers->workers;
+        try {
+            if ($Workers != null) $Workers = $Workers->workers;
+        } catch (\Throwable $e) {
+            $Workers = null;
+        }
+
 
         $Setting = new getSetting($accountId);
         $tokenMs = $Setting->tokenMs;
@@ -31,10 +38,27 @@ class WorkerController extends Controller
         $Body_employee = $Client->get($url_employee)->rows;
         $security = [];
         //СДЕЛАТЬ ПАРАЛЕЛЬНЫЙ ЗАПРОС
+        $urls = [];
         foreach ($Body_employee as $id=>$item){
             $url_security = $url_employee.'/'.$item->id.'/security';
-            $Body_security = $Client->get($url_security)->role;
+            $urls [] = $url_security;
+            //$Body_security = $Client->get($url_security)->role;
+            //$security[$item->id] = mb_substr ($Body_security->meta->href, 53);
+        }
+
+        $pools = function (Pool $pool) use ($urls,$tokenMs){
+            foreach ($urls as $url){
+                $arrPools [] = $pool->withToken($tokenMs)->get($url);
+            }
+            return $arrPools;
+        };
+
+        $responses = Http::pool($pools);
+        $count = 0;
+        foreach ($Body_employee as $id=>$item){
+            $Body_security = $responses[$count]->object()->role;
             $security[$item->id] = mb_substr ($Body_security->meta->href, 53);
+            $count++;
         }
 
         return view('setting.worker', [
