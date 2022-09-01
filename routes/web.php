d1d2bf5667f3bb00876e356d664408c906c76941
Slonexx <21moriarty21@mail.ru>
2022-09-01 14:12:43 +0600

    popup creat custom

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Config\DeleteVendorApiController;
 use App\Http\Controllers\deleteData\deleteDevice;
 use App\Http\Controllers\Popup\demandController;
 use App\Http\Controllers\Popup\fiscalizationController;
+use App\Http\Controllers\Popup\salesreturnController;
 use App\Http\Controllers\Web\getSetting\BaseController;
 use App\Http\Controllers\Web\getSetting\DeviceController;
 use App\Http\Controllers\Web\getSetting\WorkerController;
@@ -14,6 +15,7 @@ use App\Http\Controllers\Web\postSetting\postWorkerController;
 use App\Http\Controllers\Web\settingController;
 use App\Http\Controllers\Widget\customerorder\customerorderEditController;
 use App\Http\Controllers\Widget\demandEditController;
+use App\Http\Controllers\Widget\salesreturnEditController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('delete/{accountId}', [DeleteVendorApiController::class, 'delete']);
@@ -24,6 +26,7 @@ Route::get('/{accountId}/', [indexController::class, 'indexShow'])->name('main')
 
 Route::get('/widget/customerorder', [customerorderEditController::class, 'customerorder']);
 Route::get('/widget/demand', [demandEditController::class, 'demand']);
+Route::get('/widget/salesreturn', [salesreturnEditController::class, 'salesreturn']);
 
 Route::get('/Popup/customerorder', [fiscalizationController::class, 'fiscalizationPopup']);
 Route::get('/Popup/customerorder/show', [fiscalizationController::class, 'ShowFiscalizationPopup']);
@@ -33,6 +36,10 @@ Route::get('/Popup/customerorder/closeShift', [fiscalizationController::class, '
 Route::get('/Popup/demand', [demandController::class, 'DemandPopup']);
 Route::get('/Popup/demand/show', [demandController::class, 'ShowDemandPopup']);
 Route::get('/Popup/demand/send', [demandController::class, 'SendDemandPopup']);
+
+Route::get('/Popup/salesreturn', [salesreturnController::class, 'salesreturnPopup']);
+Route::get('/Popup/salesreturn/show', [salesreturnController::class, 'ShowSalesreturnPopup']);
+Route::get('/Popup/salesreturn/send', [salesreturnController::class, 'SendSalesreturnPopup']);
 
 
 
