465189005da6aae285cc8de269dfad7b5e64308e
Slonexx <21moriarty21@mail.ru>
2022-09-02 13:57:11 +0600

    edit

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -1,10 +1,14 @@
 @extends('layout')
 
 @section('content')
-    @if ( request()->isAdmin != null and request()->isAdmin != 'ALL' )
-    <div class="mt-2 alert alert-danger alert-dismissible fade show in text-center  "> Доступ к настройкам есть только у администратора
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+
+    <div class="content p-4 mt-2 bg-white text-Black rounded">
+        @if ( request()->isAdmin != null and request()->isAdmin != 'ALL' )
+            <div class="mt-2 alert alert-danger alert-dismissible fade show in text-center  "> Доступ к настройкам есть только у администратора
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        @endif
+
     </div>
-    @endif
 @endsection
 
