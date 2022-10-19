168ae8df852219f240741fe499a3b60c5bf72db1
Slonexx <21moriarty21@mail.ru>
2022-10-19 20:13:09 +0600

    edit

--- resources/views/popup/fiscalization.blade.php
+++ resources/views/popup/fiscalization.blade.php
@@ -279,7 +279,7 @@
         }
 
         function ShowCheck(){
-            let urlrekassa = 'https://rekassa.kz/'
+            let urlrekassa = 'https://app.rekassa.kz'
             //let url = 'http://rekassa/Popup/customerorder/closeShift';
             let url = 'https://smartrekassa.kz/api/ticket';
             let params = {
