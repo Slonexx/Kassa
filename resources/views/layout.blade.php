9af301ce01d82c13792e49bbf05884b4d0beaaaa
Slonexx <21moriarty21@mail.ru>
2022-12-07 18:30:51 +0600

    new update

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,59 +4,59 @@
 <body style="background-color:#dcdcdc;">
 
 <div class="page headfull">
-        <div class="sidenav">
+    <div class="sidenav">
 
-            <div class="p-2 gradient ">
-                <div class="row text-white">
-                    <div class="col-2">
-                        <img src="https://app.rekassa.kz/static/logo.png" width="35" height="35"  alt="">
-                    </div>
-                    <div class="mt-1 col-10">
-                        <label class="s-min-16"> reKassa </label>
-                    </div>
-
+        <div class="p-2 gradient ">
+            <div class="row text-white">
+                <div class="col-2">
+                    <img src="https://app.rekassa.kz/static/logo.png" width="35" height="35"  alt="">
                 </div>
-            </div>
-            <br>
-                <div class="toc-list-h1">
-                    <a id="link_1" class="mt-2 mb-2" href="/{{$accountId}}?isAdmin={{ request()->isAdmin }}">Главная </a>
-                    <div>
-                        @if ( request()->isAdmin == null )
-                        @else
-                            @if( request()->isAdmin == 'ALL')
-                                    <button id="btn_1" class="dropdown-btn">Настройки <i class="fa fa-caret-down"></i> </button>
-                                    <div class="dropdown-container">
-                                        <a id="link_2" class="mt-1" href="/Setting/Device/{{$accountId}}?isAdmin={{ request()->isAdmin }}"> Кассовый аппарат </a>
-                                        <a id="link_3" class="mt-1" href="/Setting/Document/{{$accountId}}?isAdmin={{ request()->isAdmin }}"> Документ </a>
-                                        <a id="link_4" class="mt-1" href="/Setting/Worker/{{$accountId}}?isAdmin={{ request()->isAdmin }}"> Сотрудники </a>
-                                    </div>
-                            @endif
-                        @endif
-                    </div>
-                    <a id="link_6" class="mt-1" href="/kassa/change/{{$accountId}}?isAdmin={{ request()->isAdmin }}"> Смена </a>
+                <div class="mt-1 col-10">
+                    <label class="s-min-16"> reKassa </label>
                 </div>
 
-            <div class="mt-2 mb-2" >
-                <button class="dropdown-btn">Помощь <i class="fa fa-caret-down"></i> </button>
-                    <div class="dropdown-container">
-                        <a target="_blank" href="https://smartrekassa.bitrix24.site/contact/">
-                            <i class="fa-solid fa-address-book"></i>
-                            Контакты </a>
-                        <a target="_blank" href="https://api.whatsapp.com/send/?phone=77232400545&text=" >
-                            <i class="fa-brands fa-whatsapp"></i>
-                            Написать на WhatsApp </a>
-                        <a target="_blank" href="https://smartrekassa.bitrix24.site/instruktsiiponastroyke" >
-                            <i class="fa-solid fa-chalkboard-user"></i>
-                             Инструкция </a>
-                    </div>
             </div>
-
+        </div>
+        <br>
+        <div class="toc-list-h1">
+            <a id="link_1" class="mt-2 mb-2" href="/{{$accountId}}?isAdmin={{ request()->isAdmin }}">Главная </a>
+            <div>
+                @if ( request()->isAdmin == null )
+                @else
+                    @if( request()->isAdmin == 'ALL')
+                        <button id="btn_1" class="dropdown-btn">Настройки <i class="fa fa-caret-down"></i> </button>
+                        <div class="dropdown-container">
+                            <a id="link_2" class="mt-1" href="/Setting/Device/{{$accountId}}?isAdmin={{ request()->isAdmin }}"> Кассовый аппарат </a>
+                            <a id="link_3" class="mt-1" href="/Setting/Document/{{$accountId}}?isAdmin={{ request()->isAdmin }}"> Документ </a>
+                            <a id="link_4" class="mt-1" href="/Setting/Worker/{{$accountId}}?isAdmin={{ request()->isAdmin }}"> Сотрудники </a>
+                        </div>
+                    @endif
+                @endif
+            </div>
+            <a id="link_6" class="mt-1" href="/kassa/change/{{$accountId}}?isAdmin={{ request()->isAdmin }}"> Смена </a>
         </div>
 
-        <div class="main head-full">
-                @yield('content')
+        <div class="mt-2 mb-2" >
+            <button class="dropdown-btn">Помощь <i class="fa fa-caret-down"></i> </button>
+            <div class="dropdown-container">
+                <a target="_blank" href="https://smartrekassa.bitrix24.site/contact/">
+                    <i class="fa-solid fa-address-book"></i>
+                    Контакты </a>
+                <a target="_blank" href="https://api.whatsapp.com/send/?phone=77232400545&text=" >
+                    <i class="fa-brands fa-whatsapp"></i>
+                    Написать на WhatsApp </a>
+                <a target="_blank" href="https://smartrekassa.bitrix24.site/instruktsiiponastroyke" >
+                    <i class="fa-solid fa-chalkboard-user"></i>
+                    Инструкция </a>
+            </div>
         </div>
+
     </div>
+
+    <div class="main head-full">
+        @yield('content')
+    </div>
+</div>
 
 </body>
 </html>
