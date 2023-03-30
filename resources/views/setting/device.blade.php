e60f3a51aad0df359c8fc6e20c5eb00dbe7e9767
Slonexx <21moriarty21@mail.ru>
2023-03-30 13:16:55 +0600

    new

--- resources/views/setting/device.blade.php
+++ resources/views/setting/device.blade.php
@@ -129,11 +129,10 @@
     <div class="content p-4 mt-2 bg-white text-Black rounded">
 
         <div class="row gradient rounded p-2 pb-2" style="margin-top: -1rem">
-            <div class="col-8" style="margin-top: 1.2rem"> <span class="text-white" style="font-size: 20px"> Настройки &#8594; Кассовый аппарат </span> </div>
-            <div class="col-2 mt-3"><button onclick="add_device()" type="button" class=" btn transparent btn-outline-warning"> <i class="fa-solid fa-circle-plus"></i> Добавить </button> </div>
-            <div class="col-2 text-center">
-                <img src="https://smarttis.kz/Config/logo.png" width="40%"  alt="">
-                <div class="text-white" style="font-size: 11px; margin-top: 8px"> Топ партнёр сервиса МойСклад </div>
+            <div class="col-7" style="margin-top: 1rem"> <span class="text-white" style="font-size: 20px"> Настройки &#8594; Кассовый аппарат </span> </div>
+            <div class="col-2 mt-3 text-center"><button onclick="add_device()" type="button" class=" btn transparent btn-outline-warning"> <i class="fa-solid fa-circle-plus"></i> Добавить </button> </div>
+            <div class="col-3 text-center">
+                <img src="https://dev.smartrekassa.kz/logoHeadWhite.png" width="100%"  alt="">
             </div>
         </div>
 
